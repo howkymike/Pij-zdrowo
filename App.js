c07e6dbc25f6af484a1b131a1880c81e652bef21
florkie <florekbartlomiej00@gmail.com>
2022-11-23 13:13:09 +0100

    Added drawer navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Login from "./screens/Login";
 import Register from "./screens/Register.js";
@@ -8,20 +8,22 @@ import Home from "./screens/Home.js";
 import UserProfile from "./screens/UserProfile.js";
 import PHWater from "./screens/PHWater.js";
 
-const Stack = createNativeStackNavigator();
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="UserProfile" component={UserProfile} />
-          <Stack.Screen name="PHWater" component={PHWater} />
-        </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Login" drawerPosition="right">
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="PHWater" component={PHWater} />
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="UserProfile" component={UserProfile} />
+        <Drawer.Screen name="Register" component={Register} />
+      </Drawer.Navigator>
       </NavigationContainer>
     </>
   );
