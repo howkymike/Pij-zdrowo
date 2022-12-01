c6878d109c1d77de87aa65e37a38978affa2a587
florkie <florekbartlomiej00@gmail.com>
2022-12-01 15:05:12 +0100

    added logout button

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import {useContext} from "react";
 import {StatusBar} from "expo-status-bar";
 import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem} from "@react-navigation/drawer";
+import {SafeAreaView, View, Text} from "react-native";
+
 
 import Login from "./screens/Login";
 import Register from "./screens/Register.js";
@@ -13,6 +15,24 @@ import AuthContextProvider, {AuthContext} from "./store/auth-context.js";
 import URLContextProvider from "./store/url-context.js";
 
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+    const authCtx = useContext(AuthContext);
+    return (
+        
+        <SafeAreaView style={{flex: 1}} forceInset={{top: "always", horizontal: "never"}}>
+            <DrawerContentScrollView {...props}>
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+            <View>
+                <DrawerItem label={() => <Text style={{ color: 'white' }}>Logout</Text>}
+                            style={{ backgroundColor: "cornflowerblue"}}
+                            onPress={() => alert('Logged out!') }
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
 
 export default function App() {
     return (
@@ -38,11 +58,12 @@ function AuthStack() {
 
 function AuthenticatedStack() {
     return (
-        <Drawer.Navigator initialRouteName="Login" drawerPosition="right">
+        <Drawer.Navigator initialRouteName="Login" drawerPosition="right" drawerContent={props => <CustomDrawerContent {...props} />}>
             <Drawer.Screen name="Home" component={Home}/>
             <Drawer.Screen name="PHWater" component={PHWater}/>
             <Drawer.Screen name="UserProfile" component={UserProfile}/>
             <Drawer.Screen name="Statistics" component={Statistics}/>
+            
         </Drawer.Navigator>
     );
 }
