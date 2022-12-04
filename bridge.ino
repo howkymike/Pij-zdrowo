048ae24fcb01cf6d48480ea11bbf9cfc4511688c
Mtyl <62018285+Mtyl@users.noreply.github.com>
2022-12-04 17:16:43 +0100

    Komentarze do bridge.ino

--- bridge.ino
+++ bridge.ino
@@ -3,23 +3,23 @@
 GravityTDS gravityTds;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Włączenie komunikacji szeregowej niezbędne dla działania Bridge
   pinMode(13,OUTPUT);
   digitalWrite(13, LOW);
-  Bridge.begin();
+  Bridge.begin(); // Uruchomienie Bridge pozwalającego na przekazywanie zmiennych między kontrolerem a procesroem
   digitalWrite(13, HIGH);
-  gravityTds.setPin(0);
-  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-  gravityTds.begin();  //initialization
+  gravityTds.setPin(0); // Pin do którego jest podpięta płytka od czujnika TDS
+  gravityTds.setAref(5.0);  // maksymalne napięcie odczytu ADC
+  gravityTds.setAdcRange(1024);  // rozdzielczość ADC
+  gravityTds.begin();  // inicjalizacja
 }
 
 void loop() {
-  gravityTds.setTemperature(25.0);  // set the temperature and execute temperature compensation
-  gravityTds.update();
-  float tds = gravityTds.getTdsValue();
-  float pH = analogRead(1)*5.0*3.5/1024;
-  Bridge.put("TDS",String(tds));
+  gravityTds.setTemperature(25.0);  // kompensacja dla temperatury pokojowej
+  gravityTds.update(); // Odczyt napięcia z TDS
+  float tds = gravityTds.getTdsValue(); // Przetworzenie odczytu na wartość PPM
+  float pH = analogRead(1)*5.0*3.5/1024; // Odczyt napięcia z pinu płytki pH i konwersja na wartość pH
+  Bridge.put("TDS",String(tds)); // Przekazanie wartości do mikroprocesora
   Bridge.put("pH",String(pH));
   delay(50);
 }
